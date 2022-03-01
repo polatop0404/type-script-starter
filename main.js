c5f1b98faa05080c21c60eb1a75eb35b815f37b5
MatirzayevFurqat <Matirzayevv@gmail.com>
2022-03-01 18:30:06 +0500

    tayp scrpt ni qoshdim

---
+++ main.js
@@ -0,0 +1,39 @@
+let a = 5;
+console.log(a*4);
+let r = 50;
+console.log(r*2);
+let pi=3.14;
+r=50
+console.log('l='+2*pi*r);
+console.log('s='+pi*r**2);
+a=5
+let b=6
+console.log((a+b)/2);
+console.log((a*b)**1/2);
+a=6
+b=8
+console.log('p='+2*(a+b));
+console.log('s='+a*b);
+a = 100
+b = 300
+let s1=a*b
+c=10
+d=25
+let s2=c*d
+console.log('s='+s1/s2);
+let m=11000
+let x=0.5
+console.log(m*x);
+a=11000
+b=15000
+c=22000
+x= prompt('oimadan qancha olmoqchisiz')
+let y= prompt('pechina qancha olmoqchisiz')
+let z= prompt('qanfet qancha olmoqchisiz')
+let ax=a*x
+let by=a*y
+let cz=c*z
+console.log('olma',ax);
+console.log('pechina',by);
+console.log('qonfet',cz);
+console.log('hamasi2',ax+by+cz2);
